19718d3dc7152446a5639d739610ea6dbecd2fa6
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-07-31 20:13:02 +0530

    Commit changes...

---
+++ context-api-demo/src/MyComponent.js
@@ -0,0 +1,17 @@
+// MyComponent.js
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
+export const MyComponent = () => {
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+
+    <div style={{ background: theme === 'light' ? 'white' : 'black', color: theme === 'light' ? 'black' : 'white' }}>
+      <h1>Current Theme: {theme}</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+    
+  );
+};
