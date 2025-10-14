3168987cc65e93f294e43851dfd259d50ef341e0
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-10-14 19:00:37 +0530

    Commit changes...

--- error-handling-app/src/components/BuggyComponent.js
+++ error-handling-app/src/components/BuggyComponent.js
@@ -3,16 +3,21 @@
 import React, { useState } from "react";
 
 const BuggyComponent = () => {
+
     const [count, setCount] = useState(0);
 
     const handleClick = () => {
+
         if (count >= 2) {
+
             throw new Error("You clicked too many times! ");
         }
+
         setCount(count + 1);
     };
 
     return (
+
         <div style={{ marginLeft: "20px", marginTop: "20px" }}>
             <div style={{ display: "flex", flexDirection: "column",
                 alignItems: "flex-start", gap: "20px" }}>
@@ -23,6 +28,7 @@ const BuggyComponent = () => {
                 </button>
             </div>
         </div>
+        
     );
 };
 
